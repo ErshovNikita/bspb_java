6ba48387bea0075299392a7645378e02ffe65885
ErshovNikita <marvaloredl@gmail.com>
2021-05-26 23:04:05 +0300

    I added an input from keyboard

--- src/ru/bspb/lesson3/BS.java
+++ src/ru/bspb/lesson3/BS.java
@@ -1,11 +1,15 @@
 package ru.bspb.lesson3;
 
+import java.util.Scanner;
+
 public class BS {
     public static void main(String[] args) {
         int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         int low = 1;
         int high = list.length;
-        int find = 15;
+        Scanner f = new Scanner(System.in);
+        System.out.println("Enter number from 1 to 16.");
+        int find = f.nextInt();
         if (find>high) {
             System.out.println("Your number is " + find +" It is bigger then the highest number in our range.");
         }
